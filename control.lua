785b9ae9da31473214b6ed260e00708ea5f87318
Tom Burrows <tomburrows13@icloud.com>
2022-12-24 14:35:07 +0000

    Comment type

--- control.lua
+++ control.lua
@@ -71,7 +71,7 @@ local function create_permission_group()
   group.set_allows_action(defines.input_action.inventory_split, false)
   group.set_allows_action(defines.input_action.inventory_transfer, false)
   group.set_allows_action(defines.input_action.open_item, false)  -- ?
-  group.set_allows_action(defines.input_action.open_mod_item, false)  -- L
+  group.set_allows_action(defines.input_action.open_mod_item, false)  -- ?
   group.set_allows_action(defines.input_action.open_parent_of_opened_item, false)  -- ?
   group.set_allows_action(defines.input_action.place_equipment, false)
   group.set_allows_action(defines.input_action.stack_split, false)
