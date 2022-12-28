6cfdb5ffd9df0bde4cec4a6450b4ef5c8055444d
Tom Burrows <tomburrows13@icloud.com>
2022-12-28 04:11:51 +0000

    Implement entity-paste from any range

--- control.lua
+++ control.lua
@@ -5,6 +5,29 @@ local entity_type_blacklist = {
   ["electric-pole"] = true,
 }
 
+local function increase_range(player)
+  if player.character and player.character_reach_distance_bonus < 200000 then
+    player.character_reach_distance_bonus = player.character_reach_distance_bonus + 200000
+  end
+end
+
+local function reset_range(player)
+  if not player.character then return end
+  while player.character_reach_distance_bonus >= 200000 do
+    player.character_reach_distance_bonus = player.character_reach_distance_bonus - 200000
+  end
+end
+
+local function can_reach_entity(player, entity)
+  -- Check if player can reach entity disregarding whatever reach bonus we have given the player
+  if not player.character then return end
+  local reach_distance_bonus = player.character_reach_distance_bonus
+  reset_range(player)
+  local can_reach = player.can_reach_entity(entity)
+  player.character_reach_distance_bonus = reach_distance_bonus
+  return can_reach
+end
+
 script.on_event("rc-open-gui",
   function(event)
     local player = game.get_player(event.player_index)
@@ -15,14 +38,12 @@ script.on_event("rc-open-gui",
     if selected then
       if entity_type_blacklist[selected.type] then return end
       reset_player(player)  -- Ensures that can_reach_entity is accurate
-      local out_of_reach = not player.can_reach_entity(selected)
+      local out_of_reach = not can_reach_entity(player, selected)
       local map_mode = player.render_mode == defines.render_mode.chart_zoomed_in
       if out_of_reach or map_mode then
         player.opened = nil  -- Triggers on_gui_closed before we open the GUI we care about
         if out_of_reach then
-          if player.character and player.character_reach_distance_bonus < 200000 then
-            player.character_reach_distance_bonus = player.character_reach_distance_bonus + 200000
-          end
+          increase_range(player)
           player.permission_group = game.permissions.get_group("Remote Configuration GUI opened")
         end
         player.opened = selected
@@ -37,10 +58,7 @@ script.on_event("rc-open-gui",
 
 function reset_player(player)
   player.permission_group = game.permissions.get_group("Default")
-  if not player.character then return end
-  while player.character_reach_distance_bonus >= 200000 do
-    player.character_reach_distance_bonus = player.character_reach_distance_bonus - 200000
-  end
+  reset_range(player)
 end
 script.on_event(defines.events.on_gui_closed,
   function(event)
@@ -49,8 +67,53 @@ script.on_event(defines.events.on_gui_closed,
   end
 )
 
-local function create_permission_group()
+script.on_event("rc-paste-entity-settings",
+  function(event)
+    local player = game.get_player(event.player_index)
+    local cursor_stack = player.cursor_stack
+    if cursor_stack and cursor_stack.valid_for_read then return end
+
+    local selected = player.selected
+    if not selected then return end
+
+    local in_reach = can_reach_entity(player, selected)
+    if in_reach then return end  -- Let vanilla handle this
+
+    local entity_copy_source = player.entity_copy_source
+    if not entity_copy_source then return end
+
+    removed_items = selected.copy_settings(entity_copy_source, player)
+    local surface = selected.surface
+    local position = selected.position
+    local force = player.force
+    for name, count in pairs(removed_items) do
+      surface.spill_item_stack(
+        position,
+        {name = name, count = count},
+        true,  -- enabled_looted
+        force,  -- force for deconstruction
+        false  -- allow_on_belts
+      )
+    end
+  end
+)
+
+
+local function create_permission_group(config_changed_data)
   local permissions = game.permissions
+
+  if config_changed_data then
+    -- in on_configuration_changed
+    if config_changed_data.mod_changes and config_changed_data.mod_changes["RemoteConfiguration"] then
+      for _, player in pairs(game.players) do
+        reset_player(player)
+        player.opened = nil
+      end
+    end
+    if permissions.get_group("Remote Configuration GUI opened") then
+      permissions.get_group("Remote Configuration GUI opened").destroy()
+    end
+  end
   if permissions.get_group("Remote Configuration GUI opened") then
     return
   end
@@ -76,7 +139,7 @@ local function create_permission_group()
   group.set_allows_action(defines.input_action.stack_split, false)
   group.set_allows_action(defines.input_action.stack_transfer, false)
   group.set_allows_action(defines.input_action.take_equipment, false)
-
+  group.set_allows_action(defines.input_action.paste_entity_settings, false)
 end
 
 script.on_init(create_permission_group)
--- data.lua
+++ data.lua
@@ -3,6 +3,13 @@ data:extend{
     type = "custom-input",
     name = "rc-open-gui",
     key_sequence = "",
-    linked_game_control = "open-gui"
+    linked_game_control = "open-gui",
   },
+  {
+    type = "custom-input",
+    name = "rc-paste-entity-settings",
+    key_sequence = "",
+    linked_game_control = "paste-entity-settings",
+  },
+
 }
